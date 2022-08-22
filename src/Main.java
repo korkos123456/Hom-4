d98d95a608709920e5e578bc9f34299d0cd0b3c6
korko <korkos@list.ru>
2022-08-22 20:31:13 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -41,7 +41,36 @@ public class Main {
 
         }
 
+    public static void task4() {
+        short bottomLath = 250;
+        short topLath = 500;
+        byte toShredWeight = 7;
+        int gramsInKg = 1000;
+        double daysWithBottomLath = toShredWeight * Math.ceil((double) gramsInKg / bottomLath);
+        System.out.println((int) daysWithBottomLath + " дней потребуется, если сбрасывать по " + bottomLath + " граммов в день");
+        double daysWithTopLath = toShredWeight * Math.ceil((double) gramsInKg / topLath);
+        System.out.println((int) daysWithTopLath + " дней потребуется, если сбрасывать по " + topLath + " граммов в день");
+        double averageDaysTake = Math.ceil((daysWithTopLath + daysWithBottomLath) / 2);
+        System.out.println("В среднем, чтобы скинуть " + toShredWeight + " кг, потребуется " + (int) averageDaysTake + " день");
+        {
 
+            public static void task3() {
+
+                byte numberOfBananas = 5;
+                short bananaWeight = 80;
+                short milkPortion = 200 / 100;
+                short milkPortionWeight = 105;
+                byte countOfIceCreamPackages = 2;
+                short iceCreamPackageWeight = 100;
+                byte countOfEggs = 4;
+                byte eggWeight = 70;
+                int gramsInKg = 1000;
+                int milkWeight = milkPortion * milkPortionWeight;
+                int totalBreakfastWeight = numberOfBananas * bananaWeight + milkWeight + countOfIceCreamPackages * iceCreamPackageWeight + countOfEggs * eggWeight;
+                double reducedWeight = (double) totalBreakfastWeight / gramsInKg;
+
+                System.out.println("Вес спорт-завтрака составил " + reducedWeight + " кг.");
+        }
 
 
 
